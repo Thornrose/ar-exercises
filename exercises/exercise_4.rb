f7ce85ec424e54d2df8f4516c2e1b545ae132d7a
Cheever Esler <ames.esler@gmail.com>
2022-12-13 13:07:17 -0400

    add exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,28 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# Borrowing and modifying the code from Exercise one, create 3 more stores:
+# Surrey (annual_revenue of 224000, carries women's apparel only)
+# Whistler (annual_revenue of 1900000 carries men's apparel only)
+# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+# Using the where class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable @mens_stores.
+# Loop through each of these stores and output their name and annual revenue on each line.
+# Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+
+Surrey   = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Whistler  = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Yaletown   = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+puts Store.count
+
+mens_stores = Store.where(mens_apparel: true)
+  puts "Men's clothing stores:"
+mens_stores.each do |store|
+  puts "Store location: #{store.name} Annual revenue: #{store.annual_revenue}"
+end
+
+womens_budget_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+puts "women's affordable clothing stores:"
+womens_budget_stores.each do |store|
+  puts "Store location: #{store.name} Annual revenue: #{store.annual_revenue}"
+end
